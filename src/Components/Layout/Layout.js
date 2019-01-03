1df28bb38fbc9a3ecee86d593ffe6f6550703425
Anne Thor <anne.thor@gmail.com>
2019-01-02 21:19:20 -0800

    completed basic burger builder app setup

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../HOC/Aux';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import classes from './Layout.css';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-      </main>
-  </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSideDrawer: false,
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerTogglerHandler = () => {
+    this.setState( (prevState) => {
+      return{ showSideDrawer: !prevState.showSideDrawer };
+    });
+  }
+
+  render() {
+      return (
+        <Aux>
+          <Toolbar drawerTogglerClicked={this.sideDrawerTogglerHandler}/>
+          <SideDrawer closed={this.sideDrawerClosedHandler}
+                      open={this.state.showSideDrawer}/>
+          <main className={classes.Content}>
+            {this.props.children}
+            </main>
+        </Aux>
+      )
+  }
+}
+
+export default Layout;
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from "./Toolbar.css";
+import DrawerToggler from "../SideDrawer/DrawerToggler/DrawerToggler";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggler clicked={props.drawerTogglerClicked}/>
     <div className={classes.Logo}><Logo /></div>
     <nav className={classes.DesktopOnly}>
       <NavigationItems />
